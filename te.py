44e55ab36ff7a07fc4b3133323d108df6cd85653
Imaduddin Abdul Karim <74599441+imadaka19@users.noreply.github.com>
2025-05-23 10:16:56 +0700

    tes

---
+++ te.py
@@ -0,0 +1,766 @@
+chat_text = '''
+[18:23, 09/05/2025] Gazza mt lion: LOC: K245
+BIN: BOX 0310
+PN: TMS4E
+QTY: 4 EA
+REMARK: SURPLUS 3 EA, FOUND 4 BUT ONLY NOT FOUND 1 EA LEFT
+[18:29, 09/05/2025] Gazza mt lion: UNRECORD
+PN: F4LB
+SN: VN12140258
+QTY: 1 EA
+BIN FOUND: BOX 0310
+REMARK: UNRECORD, FROM EMRO BELONGS TO K102 BIN FREEZE
+[18:29, 09/05/2025] jihad hartono ACCT Lion: PN: OEX32B
+SN: -
+QTY EMRO: -
+QTY ACT: 2
+BIN ACT: BOX0310
+REMARKS: UNRECORD
+[18:29, 09/05/2025] jihad hartono ACCT Lion: PN: OEX30B
+SN: -
+QTY EMRO: -
+QTY ACT: 1
+BIN ACT: BOX0310
+REMARKS: UNRECORD
+[18:29, 09/05/2025] jihad hartono ACCT Lion: PN: OEX28B
+SN: -
+QTY EMRO: -
+QTY ACT: 1
+BIN ACT: BOX0310
+REMARKS: UNRECORD
+[18:29, 09/05/2025] jihad hartono ACCT Lion: PN: OEX26B
+SN: -
+QTY EMRO: -
+QTY ACT: 1
+BIN ACT: BOX0310
+REMARKS: UNRECORD
+[18:29, 09/05/2025] jihad hartono ACCT Lion: PN: OEX24B
+SN: -
+QTY EMRO: -
+QTY ACT: 1
+BIN ACT: BOX0310
+REMARKS: UNRECORD
+[18:29, 09/05/2025] jihad hartono ACCT Lion: PN: OEX22B
+SN: -
+QTY EMRO: -
+QTY ACT: 1
+BIN ACT: BOX0310
+REMARKS: UNRECORD
+[18:29, 09/05/2025] jihad hartono ACCT Lion: PN: OEX20B
+SN: -
+QTY EMRO: -
+QTY ACT: 1
+BIN ACT: BOX0310
+REMARKS: UNRECORD
+[18:32, 09/05/2025] Gazza mt lion: UNRECORD
+PN: RYA1618
+SN: OD/TCSZB/TB/061
+QTY: 1 EA
+BIN FOUND: BOX 0310
+REMARK: UNRECORD, RYA1618 ALL ALREADY FOUND
+[18:33, 09/05/2025] jihad hartono ACCT Lion: PN:  VO2022B
+SN: TE06141932
+QTY EMRO: -
+QTY ACT: 1
+BIN ACT: BOX0310
+REMARKS: UNRECORD
+[18:36, 09/05/2025] jihad hartono ACCT Lion: PN:  OES14B
+SN: TE06141632
+QTY EMRO: -
+QTY ACT: 1
+BIN ACT: BOX0310
+REMARKS: UNRECORD
+[18:39, 09/05/2025] jihad hartono ACCT Lion: PN:  VO2022B
+SN: TE06141946
+QTY EMRO: -
+QTY ACT: 1
+BIN ACT: BOX0310
+REMARKS: UNRECORD
+[18:40, 09/05/2025] Gugun Mangara: PN : FXLBP3
+QTY : 1 EA
+BIN : BOX 0301
+REMARKS : UNRECORD
+[18:40, 09/05/2025] Gugun Mangara: PN : TMXLBP10
+QTY : 1 EA
+BIN : BOX 0301
+REMARKS : UNRECORD
+[18:40, 09/05/2025] Gugun Mangara: PN : FXLBP10
+QTY : 1 EA
+BIN : BOX 0301
+REMARKS : UNRECORD
+[18:40, 09/05/2025] Gugun Mangara: PN : TMXLBP4
+QTY : 1 EA
+BIN : BOX 0301
+REMARKS : UNRECORD
+[18:41, 09/05/2025] jihad hartono ACCT Lion: PN:  VO1618B
+SN: TE06141781
+QTY EMRO: -
+QTY ACT: 1
+BIN ACT: BOX0310
+REMARKS: UNRECORD
+[18:42, 09/05/2025] Gugun Mangara: PN : FXLBP6
+QTY : 2 EA
+BIN : BOX 0301
+REMARKS : UNRECORD
+[18:42, 09/05/2025] Gugun Mangara: PN : TMXLBP6
+QTY : 1 EA
+BIN : BOX 0301
+REMARKS : UNRECORD
+[18:42, 09/05/2025] jihad hartono ACCT Lion: PN:  OES12B
+SN: TE06141502
+QTY EMRO: -
+QTY ACT: 1
+BIN ACT: BOX0310
+REMARKS: UNRECORD
+[18:43, 09/05/2025] Eto acct hsy: UNRECORD
+
+LOC: K245
+BIN: BOX0310
+PN: RBZ1214
+SN: TE06140879
+[18:43, 09/05/2025] Eto acct hsy: UNRECORD
+
+LOC: K245
+BIN: BOX0310
+PN: RYA1618
+SN: -
+[18:43, 09/05/2025] Eto acct hsy: UNRECORD
+
+LOC: K245
+BIN: BOX0310
+PN: RY1214
+SN: -
+[18:43, 09/05/2025] Eto acct hsy: UNRECORD
+
+LOC: K245
+BIN: BOX0310
+PN: RYA1618
+SN: -
+[18:46, 09/05/2025] Gugun Mangara: LOC : K245
+PN : OEX10B
+QTY : 3 EA
+BIN : BOX 0301
+REMARKS : UNRECORD
+[18:50, 09/05/2025] Gazza mt lion: UNRECORD
+LOC: K245
+PN: 61ACP
+QTY: 2 EA
+BIN: BOX 0310
+REMARK: UNRECORD INSIDE BOX 0310
+[18:51, 09/05/2025] Gazza mt lion: UNRECORD
+LOC: K245
+BIN: BOX 0310
+PN: FR80
+QTY: 1 EA
+REMARK: UNRECORD IN BOX 0310
+[18:53, 09/05/2025] Gazza mt lion: UNRECORD
+LOC: K245
+BIN: BOX 0310
+PN: SGDP42IRB
+SN: OD/KUL/AM/048/NA2
+REMARK: UNRECORD IN BOX 0310
+[19:00, 09/05/2025] Gazza mt lion: UNRECORD
+LOC: K245
+BIN: BOX 0310
+PN: TMD11
+SN: OD 17 B1 007
+REMARK: UNRECORD IN BOX 0310
+[19:00, 09/05/2025] Gugun Mangara: LOC : K245
+PN : FC028A
+QTY : 1 EA
+BIN : BOX 0301
+REMARKS : UNRECORD
+[19:00, 09/05/2025] Gugun Mangara: LOC : K245
+PN : F081
+QTY : 1 EA
+BIN : BOX 0301
+REMARKS : UNRECORD
+[19:00, 09/05/2025] Gugun Mangara: LOC : K245
+PN : F141
+QTY : 1 EA
+BIN : BOX 0301
+REMARKS : UNRECORD
+[19:00, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : SGDMRC11A0
+REMARK : UNRECORD BOX0310
+[19:00, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : FCO14A
+QTY : 2 EA
+REMARK : UNRECORD BOX0310
+[19:00, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : FCO16A
+QTY : 2 EA
+REMARK : UNRECORD BOX0310
+[19:00, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : FCO18A
+QTY : 2 EA
+REMARK : UNRECORD BOX0310
+[19:00, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : FCO20A
+QTY : 2 EA
+REMARK : UNRECORD BOX0310
+[19:00, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : FCO22A
+QTY : 2 EA
+REMARK : UNRECORD BOX0310
+[19:00, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : FCO24A
+QTY : 1 EA
+REMARK : UNRECORD BOX0310
+[19:00, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : FCO26A
+QTY : 2 EA
+REMARK : UNRECORD BOX0310
+[19:00, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : FCO28A
+QTY : 1 EA
+REMARK : UNRECORD BOX0310
+[19:00, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : FCO30A
+QTY : 2 EA
+REMARK : UNRECORD BOX0310
+[19:00, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : FCO32
+QTY : 1 EA
+REMARK : UNRECORD BOX0310
+[19:00, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : FC34B
+QTY : 1 EA
+REMARK : UNRECORD BOX0310
+[19:00, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : FC36A
+QTY : 4 EA
+REMARK : UNRECORD BOX0310
+[19:00, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : FC38A
+QTY : 2 EA
+REMARK : UNRECORD BOX0310
+[19:00, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : TR72
+QTY : 1 EA
+REMARK : UNRECORD BOX0310
+[19:00, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : 96ACF
+QTY : 1 EA
+REMARK : UNRECORD BOX0310
+[19:01, 09/05/2025] jihad hartono ACCT Lion: PN:  ST50A
+SN: VN12140195
+QTY EMRO: -
+QTY ACT: 1
+BIN ACT: BOX0310
+REMARKS: UNRECORD
+[19:02, 09/05/2025] jihad hartono ACCT Lion: PN:  ST50A
+SN: -
+QTY EMRO: -
+QTY ACT: 4
+BIN ACT: BOX0310
+REMARKS: UNRECORD
+[19:05, 09/05/2025] Gazza mt lion: UNRECORD
+LOC: K245
+BIN: BOX 0310
+PN: 63997
+SN: OD/17/TB14/042
+REMARK: UNRECORD IN BOX 0310
+[19:07, 09/05/2025] Gazza mt lion: UNRECORD
+LOC: K245
+BIN: BOX 0310
+PN: OES12B
+SN: OD 17 010
+REMARK: UNRECORD IN BOX 0310
+[19:12, 09/05/2025] Gugun Mangara: LOC : K245
+PN : TMD16
+QTY : 1 EA
+BIN : BOX 0301
+REMARKS : UNRECORD
+[19:13, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : AN850810B
+QTY EMRO : 1 EA
+QTY ACT : 5 EA
+REMARK : SURPLUS 4 EA
+[19:13, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : AN85089B
+QTY : 6 EA
+REMARK : UNRECORD BOX0310
+[19:13, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : AN850812B
+QTY : 6 EA
+REMARK : UNRECORD BOX0310
+[19:13, 09/05/2025] Imaduddin Abdul Karim: LOC : K245
+BIN : BOX0310
+PN : AN850811B
+QTY : 6 EA
+REMARK : UNRECORD BOX0310
+[19:14, 09/05/2025] Daniel Marselinus Acct HSY: LOC: K245
+BIN: BOX 0310
+PN: SGDP42IRB
+QTY: 1
+REMARK: UNRECORD
+[19:14, 09/05/2025] Daniel Marselinus Acct HSY: LOC: K245
+BIN: BOX 0310
+PN: SGDP31IRB
+QTY: 1
+REMARK: UNRECORD
+[19:15, 09/05/2025] Daniel Marselinus Acct HSY: LOC: K245
+BIN: BOX 0310
+PN: SGD6B
+QTY: 2
+REMARK: UNRECORD
+[19:15, 09/05/2025] Gazza mt lion: UNRECORD
+LOC: K245
+BIN: EOB TOOL BOX 1
+PN: TMD11
+SN: VN12140195
+REMARK: UNRECORD IN EOB TOOL BOX 1
+[19:16, 09/05/2025] Daniel Marselinus Acct HSY: LOC: K245
+BIN: BOX 0310
+PN: SGD2B
+QTY: 2
+REMARK: UNRECORD
+[19:17, 09/05/2025] Daniel Marselinus Acct HSY: LOC: K245
+BIN: BOX 0310
+PN: SGD4B
+QTY: 2
+REMARK: UNRECORD
+[19:17, 09/05/2025] Daniel Marselinus Acct HSY: LOC: K245
+BIN: BOX 0310
+PN: SGD4B
+QTY: 1
+REMARK: UNRECORD
+[19:17, 09/05/2025] Daniel Marselinus Acct HSY: LOC: K245
+BIN: BOX 0310
+PN: SGD8B
+QTY: 1
+REMARK: UNRECORD
+[19:32, 09/05/2025] jihad hartono ACCT Lion: PN: SGDMRC4-40
+SN: -
+BIN: EOB TOOL BOX 1
+REMARSK: UNRECORDS
+[19:32, 09/05/2025] jihad hartono ACCT Lion: PN: T72
+SN: -
+BIN: EOB TOOL BOX 1
+REMARSK: UNRECORDS
+[19:33, 09/05/2025] jihad hartono ACCT Lion: PN: SGDMRC4A
+SN: -
+BIN: EOB TOOL BOX 1
+REMARSK: UNRECORDS
+[19:33, 09/05/2025] jihad hartono ACCT Lion: PN:
+-OEX11B
+-OEX10B
+SN: -
+BIN: EOB TOOL BOX 1
+REMARSK: UNRECORDS
+[09:57, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: RYA1618
+SN: VN12140258
+BIN: TOOL BOX 3
+REMARSK: UNRECORDS
+[09:58, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: OEX30B
+SN: VN12140256
+BIN: TOOL BOX 3
+REMARSK: UNRECORDS
+[09:58, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: FXLBP10
+SN:
+BIN: TOOL BOX 3
+REMARSK: UNRECORDS
+[09:59, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: OEX11B
+SN:
+BIN: TOOL BOX 3
+REMARSK: UNRECORDS
+[09:59, 10/05/2025] jihad hartono ACCT Lion: BIN: TOOL BOX 3
+PN: OEX24B
+SN: VN12140254
+QTY EMRO: 0
+QTY ACT: 1
+REMARKS: UNRECORD
+[09:59, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: OEX26B
+SN:
+BIN: TOOL BOX 3
+REMARSK: UNRECORDS
+[10:00, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: OEX28B
+SN:
+BIN: TOOL BOX 3
+REMARSK: UNRECORDS
+[10:00, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: OEX18B
+SN:
+BIN: TOOL BOX 3
+REMARSK: UNRECORDS
+[10:00, 10/05/2025] jihad hartono ACCT Lion: BIN: TOOL BOX 3
+PN: OEX22B
+SN: -
+QTY EMRO: 0
+QTY ACT: 1
+REMARKS: UNRECORD
+[10:01, 10/05/2025] jihad hartono ACCT Lion: BIN: TOOL BOX 3
+PN: OEX32B
+SN: -
+QTY EMRO: 0
+QTY ACT: 1
+REMARKS: UNRECORD
+[10:01, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: FXLBP6
+SN:
+BIN: TOOL BOX 3
+REMARSK: UNRECORDS
+[10:02, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: 96ACF
+SN:
+BIN: TOOL BOX OD4
+REMARSK: UNRECORDS
+[10:07, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: SGDP42IRB
+SN:
+BIN: TOOL BOX OD4
+REMARSK: UNRECORDS
+[10:08, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: SGD6B
+SN:
+BIN: TOOL BOX OD4
+REMARSK: UNRECORDS
+CCTV: 7
+FIREX: 4
+HUMADITY: 1
+FINGER ACCES: 0
+[10:27, 10/05/2025] jihad hartono ACCT Lion: LOC: K245
+PN: F111
+SN:
+BIN: TOOL BOX 1
+REMARSK: UNRECORDS
+[10:31, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: VO1618B
+SN:
+BIN: TOOL BOX 1
+REMARSK: UNRECORDS
+[10:31, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: OES10B
+SN:
+BIN: TOOL BOX 1
+REMARSK: UNRECORDS
+[10:32, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: OEX32B
+SN:
+BIN: TOOL BOX 1
+REMARSK: UNRECORDS
+[10:32, 10/05/2025] jihad hartono ACCT Lion: LOC: K245
+PN: OEX20B
+SN:
+BIN: TOOL BOX 1
+REMARSK: UNRECORDS
+[10:33, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: OEX18B
+SN:
+BIN: TOOL BOX 1
+REMARSK: UNRECORDS
+[10:33, 10/05/2025] jihad hartono ACCT Lion: LOC: K245
+PN: OEX16B
+SN:
+BIN: TOOL BOX 1
+REMARSK: UNRECORDS
+[10:35, 10/05/2025] Gugun Mangara: LOC : K245
+PN : OEX22B
+SN : -
+BIN : TOOL BOX 1
+REMARKS : UNRECORD
+[10:35, 10/05/2025] Gugun Mangara: LOC : K245
+PN : OEX26B
+SN : -
+BIN : TOOL BOX 1
+REMARKS : UNRECORD
+[10:35, 10/05/2025] Gugun Mangara: LOC : K245
+PN : OEX24B
+SN : -
+BIN : TOOL BOX 1
+REMARKS : UNRECORD
+[10:35, 10/05/2025] Gugun Mangara: LOC : K245
+PN : OEX30B
+SN : -
+BIN : TOOL BOX 1
+REMARKS : UNRECORD
+[10:35, 10/05/2025] Gugun Mangara: LOC : K245
+PN : OEX28B
+SN : -
+BIN : TOOL BOX 1
+REMARKS : UNRECORD
+[10:35, 10/05/2025] Gugun Mangara: LOC : K245
+PN : OEX14B
+SN : -
+BIN : TOOL BOX 1
+REMARKS : UNRECORD
+[10:35, 10/05/2025] Gugun Mangara: LOC : K245
+PN : OEX11B
+SN : -
+BIN : TOOL BOX 1
+REMARKS : UNRECORD
+[10:43, 10/05/2025] jihad hartono ACCT Lion: LOC: K245
+PN: OEX28B
+SN:
+BIN: TOOL BOX C
+REMARSK: UNRECORDS
+[10:43, 10/05/2025] Gugun Mangara: LOC : K245
+PN : OEX32B
+SN : -
+BIN : TOOL BOX C
+REMARKS : UNRECORD
+[10:43, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: OEX22B
+SN: 12140256
+BIN: TOOL BOX C
+REMARSK: UNRECORDS
+[10:43, 10/05/2025] Gugun Mangara: LOC : K245
+PN : OEX26B
+SN : -
+BIN : TOOL BOX C
+REMARKS : UNRECORD
+[10:44, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: OEX18B
+SN: 12140258
+BIN: TOOL BOX C
+REMARSK: UNRECORDS
+[10:45, 10/05/2025] jihad hartono ACCT Lion: LOC: K245
+PN: OEX16B
+SN: VN12140256
+BIN: TOOL BOX C
+REMARSK: UNRECORDS
+[10:45, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: OEX211B
+SN: 12140256
+BIN: TOOL BOX C
+REMARSK: UNRECORDS
+[10:45, 10/05/2025] Gugun Mangara: LOC : K245
+PN : OEX20B
+SN : VN12140258
+BIN : TOOL BOX C
+REMARKS : UNRECORD
+[10:45, 10/05/2025] Gugun Mangara: LOC : K245
+PN : OEX32B
+SN : VN12140258
+BIN : TOOL BOX C
+REMARKS : UNRECORD
+[10:45, 10/05/2025] Gugun Mangara: LOC : K245
+PN : OEX12B
+SN : -
+BIN : TOOL BOX C
+REMARKS : UNRECORD
+[10:45, 10/05/2025] Gugun Mangara: LOC : K245
+PN : OEX14B
+SN : -
+BIN : TOOL BOX C
+REMARKS : UNRECORD
+[10:46, 10/05/2025] jihad hartono ACCT Lion: LOC: K245
+PN: RYA1618
+SN:
+BIN: TOOL BOX C
+REMARSK: UNRECORDS
+[10:46, 10/05/2025] Gugun Mangara: LOC : K245
+PN : OES10B
+SN : TE0614346
+BIN : TOOL BOX C
+REMARKS : UNRECORD
+[10:47, 10/05/2025] Gugun Mangara: LOC : K245
+PN : F111
+SN : -
+BIN : TOOL BOX C
+REMARKS : UNRECORD
+[10:47, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: OEX28B
+SN: 12140256
+BIN: TOOL BOX C
+REMARSK: UNRECORDS
+[10:47, 10/05/2025] jihad hartono ACCT Lion: LOC: K245
+PN: OEX30B
+SN: VN12140258
+BIN: TOOL BOX C
+REMARSK: UNRECORDS
+[10:48, 10/05/2025] Gugun Mangara: LOC : K245
+PN : OEX26B
+SN : VN12140256
+BIN : TOOL BOX C
+REMARKS : UNRECORD
+[10:48, 10/05/2025] Gugun Mangara: LOC : K245
+PN : OEX26B
+SN : VN12140256
+BIN : TOOL BOX C
+REMARKS : UNRECORD
+[10:48, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: OEX32B
+SN: 12140256
+BIN: TOOL BOX C
+REMARSK: UNRECORDS
+[10:48, 10/05/2025] Gugun Mangara: LOC : K245
+PN : WTR1A
+SN : TE12144811
+BIN : TOOL BOX C
+REMARKS : UNRECORD
+[10:49, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: OEX12B
+SN: 12140254
+BIN: TOOL BOX C
+REMARSK: UNRECORDS
+[10:49, 10/05/2025] jihad hartono ACCT Lion: LOC: K245
+PN: OEX16B
+SN: VN12140258
+BIN: TOOL BOX C
+REMARSK: UNRECORDS
+[10:49, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: OEX8B
+SN: 12140256
+BIN: TOOL BOX C
+REMARSK: UNRECORDS
+[10:50, 10/05/2025] Gugun Mangara: LOC : K245
+PN :
+- OEX24B (SN : VN12140258)
+- ⁠OEX22B
+BIN : TOOL BOX C
+QTY : 2 EA
+REMARKS : UNRECORD
+[10:50, 10/05/2025] jihad hartono ACCT Lion: LOC: K245
+PN: OEX20B
+SN: VN12140256
+BIN: TOOL BOX C
+REMARSK: UNRECORDS
+[10:51, 10/05/2025] Gugun Mangara: LOC : K245
+PN :
+- OEX14B
+- ⁠OEX11B
+SN : -
+QTY : 2 EA
+BIN : TOOL BOX C
+REMARKS : UNRECORD
+[10:51, 10/05/2025] Imaduddin Abdul Karim: LOC: K245
+PN: OES8B
+SN: TE06141196
+BIN: TOOL BOX C
+REMARSK: UNRECORDS
+'''
+
+import re
+import pandas as pd
+from datetime import datetime
+# Regex pattern to extract message blocks
+pattern = r'\[(\d{2}:\d{2}), (\d{2}/\d{2}/\d{4})\] (.*?): (.+?)(?=\[\d{2}:\d{2},|\Z)'
+entries = re.findall(pattern, chat_text, re.DOTALL)
+
+data = []
+
+for time, date_str, sender, message in entries:
+    if not re.search(r'\b(UNRECORD|REQ BIN TF|WRONG BIN|UNRECORDS)\b', message, re.IGNORECASE):
+        continue
+
+    remark = "UNRECORD"
+    if "REQ BIN TF" in message.upper():
+        remark = "REQ BIN TF"
+    elif "WRONG BIN" in message.upper():
+        remark = "WRONG BIN"
+
+    base = {
+        "Nama": sender.split()[0],
+        "Tanggal": datetime.strptime(date_str, "%d/%m/%Y").strftime("%m/%d/%Y"),
+        "LOC": "",
+        "BIN": "",
+        "PN": "",
+        "SN": "",
+        "QTY": 1,
+        "REMARK": remark
+    }
+
+    lines = message.strip().splitlines()
+    sn_list = []
+    parsing_bulk_items = False
+    multi_item_found = False
+
+    for line in lines:
+        line = line.strip()
+        upper_line = line.upper()
+
+        if re.match(r'PN\s*(,|&)\s*QTY', upper_line):
+            parsing_bulk_items = True
+            continue
+
+        if parsing_bulk_items:
+            match = re.match(r'^\d+\.\s*([\w\-\/]+)\s*\((\d+)', line)
+            if match:
+                pn = match.group(1).strip()
+                qty = int(match.group(2).strip())
+                record = base.copy()
+                record["PN"] = pn
+                record["QTY"] = qty
+                data.append(record)
+                multi_item_found = True
+            continue
+
+        if line.startswith("-") and "|" in line:
+            item_match = re.match(r'-\s*(.+?)\s*\|\s*(\d+)', line)
+            if item_match:
+                pn = item_match.group(1).strip()
+                qty = int(item_match.group(2).strip())
+                record = base.copy()
+                record["PN"] = pn
+                record["QTY"] = qty
+                data.append(record)
+                multi_item_found = True
+            continue
+
+        if re.match(r'(PN:|PN :)', upper_line):
+            base["PN"] = line.split(":", 1)[1].strip()
+
+        elif re.match(r'(SN:|SN :)', upper_line):
+            sn = line.split(":", 1)[1].strip()
+            if sn:
+                sn_list.append(sn)
+
+        elif re.match(r'^\d+\.\s', line):  # SN list
+            sn = line.split('.', 1)[1].strip()
+            if sn:
+                sn_list.append(sn)
+
+        elif re.match(r'(BIN|BIN\s*ACT|BIN\s*ACTUAL|FOUND IN BIN)', upper_line):
+            base["BIN"] = line.split(":", 1)[1].strip()
+
+        elif re.match(r'(LOC:|LOC :)', upper_line):
+            base["LOC"] = line.split(":", 1)[1].strip()
+
+        elif "QTY ACTUAL" in upper_line or "QTY ACT" in upper_line or "QTY" in upper_line:
+            qty_match = re.search(r'\d+', line)
+            if qty_match:
+                base["QTY"] = int(qty_match.group())
+
+        elif re.match(r'(REMARK:|REMARKS:|REMARK :|REMARKS :)', upper_line):
+            base["REMARK"] = line.split(":", 1)[1].strip()
+
+    if not multi_item_found:
+        if not sn_list:
+            sn_list.append("")  # still add row if SN is empty
+        for sn in sn_list:
+            row = base.copy()
+            row["SN"] = sn
+            data.append(row)
+
+# Create DataFrame
+df2 = pd.DataFrame(data)
+# df["REMARK"] = df["REMARK"].replace("", "UNRECORD")
+
+df2
